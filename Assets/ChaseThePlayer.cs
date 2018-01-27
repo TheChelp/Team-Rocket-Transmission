792a1cb8bd820062f2c05a6fefd98a2de75ba778
Chelsea Frederick <thechelp@Chelseas-MacBook-Pro.local>
2018-01-27 13:09:19 -0800

    UI assets, player‘,‘‘ enemy AI

---
+++ Assets/ChaseThePlayer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseThePlayer : MonoBehaviour {
+
+	GameObject thePlayer = null;
+	public float SluggishTimeInterval = 0.5f;
+	public float MovementSpeed = 7;
+	public float AttackRecoveryTime = 0.5f;
+	float alertnessTimer = 0;
+	float attackRecoveryTimer = 0;
+	public Vector3 WhereToGo = new Vector3 ();
+
+	// Use this for initialization
+	void Start () {
+		thePlayer = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		attackRecoveryTimer -= Time.deltaTime;
+		alertnessTimer += Time.deltaTime;
+		if (alertnessTimer > SluggishTimeInterval) {
+			if (thePlayer != null) {
+				WhereToGo = thePlayer.transform.position;
+			}
+			alertnessTimer = 0f;
+		}
+
+		Vector3 newPosition = Vector3.MoveTowards (transform.position, WhereToGo, MovementSpeed * Time.deltaTime);
+		transform.position = newPosition;
+		transform.LookAt (WhereToGo);
+	}
+
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (attackRecoveryTimer <= 0) {
+			Debug.Log ("Hit Something");
+			if (collision.collider.tag == "Player") {
+				collision.collider.gameObject.SendMessage ("HitYou");
+				attackRecoveryTimer = AttackRecoveryTime;
+			}
+		}
+	}
+
+}
---
+++ Assets/LifeIndicator.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeIndicator : MonoBehaviour {
+
+	public int LifeLevel = 6;
+	private int LastShownLevel = -1;
+
+	public List<GameObject> TheImages = new List<GameObject> ();
+
+	private GameObject ThePlayer=null;
+
+
+	void ShowCurrentState()
+	{
+		if (LastShownLevel != 0 && LifeLevel == 0) {
+			if (ThePlayer != null)
+				ThePlayer.SendMessage ("PleaseDie");
+		}
+		
+		if (LifeLevel >= 0 && LifeLevel < NumberOfImages) {
+			foreach (GameObject imageObj in TheImages) {
+				imageObj.SetActive (false);
+			}
+			TheImages [LifeLevel].SetActive (true);
+		}
+			
+		LastShownLevel = LifeLevel;
+	}
+
+	public void SetValue(int newValue)
+	{
+		newValue = newValue % NumberOfImages;
+		LifeLevel = newValue;
+		ShowCurrentState ();
+	}
+
+	public void LoseLife()
+	{
+		Debug.Log ("Losing life");
+		if (LifeLevel > 0)
+			LifeLevel--;
+		ShowCurrentState ();
+	}
+
+	public void GainLife()
+	{
+		if (LifeLevel < NumberOfImages-1)
+			LifeLevel++;
+		ShowCurrentState ();
+	}
+
+	public void RestoreLife()
+	{
+		NumberOfImages = TheImages.Count;
+		LifeLevel = NumberOfImages-1;
+		ShowCurrentState ();
+	}
+
+	private int NumberOfImages;
+
+	// Use this for initialization
+	void Start () {
+		ThePlayer = GameObject.FindGameObjectWithTag ("Player");
+
+		NumberOfImages = TheImages.Count;
+		ShowCurrentState ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (LifeLevel != LastShownLevel) {
+			ShowCurrentState ();
+		}
+
+	}
+}
