5ba48ea52bc8fd6565c2b638787bea31a0672760
LeJon McGowan <lejonmcgowan@gmail.com>
2018-01-28 07:49:44 -0800

    the great commit sync of 8AM of the due date

--- Assets/Scripts/SpriteActions.cs
+++ Assets/Scripts/SpriteActions.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Vuforia;
+﻿using UnityEngine;
 
 public class SpriteActions : MonoBehaviour
 {
