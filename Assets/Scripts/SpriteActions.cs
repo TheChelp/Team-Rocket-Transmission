9494b1ba99435a9e4a436dddb967c9d44bd4127f
LeJon McGowan <lejonmcgowan@gmail.com>
2018-01-27 18:19:47 -0800

    get flashlight logic working

--- Assets/Scripts/SpriteActions.cs
+++ Assets/Scripts/SpriteActions.cs
@@ -1,38 +1,85 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
 public class SpriteActions : MonoBehaviour
 {
 
-    public float speed;
-    private int dx, dy;
-    
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public float Speed;
+    public float Offset = 0;
+
+    private int _dx, _dy;
+    private Light _flashLight;
+    private Vector3 _rotationAngles;
+
+
+    // Use this for initialization
+    void Start () {
+        _flashLight = gameObject.GetComponentInChildren<Light>();
+        _rotationAngles = new Vector3();
+
+        if (_flashLight != null)
+            _rotationAngles = _flashLight.transform.eulerAngles;
+
+        if (Speed == 0)
+        {
+            Debug.Log("<color=yellow> Warning: Speed is zero. Character will not move </color>");
+        }
+
+        if (_flashLight == null)
+        {
+            Debug.Log("Flashlight not found :(");
+        }
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	    if (gameObject != null)
+	void Update ()
+	{
+        if (Input.GetKey(KeyCode.W))
 	    {
-	        if (Input.GetKey(KeyCode.W))
-	            dy++;
-	        if (Input.GetKey(KeyCode.S))
-	            dy--;
-	        if (Input.GetKey(KeyCode.A))
-	            dx--;
-	        if (Input.GetKey(KeyCode.D))
-	            dx++;
-	       
-	        gameObject.transform.position += new Vector3(dx * speed, dy * speed,0);
-	        dx = 0;
-	        dy = 0;
-
+	        _dy++;
+	        if (_flashLight != null)
+	        {
+	            _flashLight.transform.eulerAngles = new Vector3(-90, _rotationAngles.y, _rotationAngles.z);
+                _flashLight.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + Offset, _flashLight.transform.position.z);
+            }
 	    }
+	    if (Input.GetKey(KeyCode.S))
+	    {
+	        _dy--;
+	        if (_flashLight != null)
+	        {
+	            _flashLight.transform.eulerAngles = new Vector3(90, _rotationAngles.y, _rotationAngles.z);
+                _flashLight.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - Offset, _flashLight.transform.position.z);
+            }
+	    }
+	    if (Input.GetKey(KeyCode.A))
+	    {
+	        _dx--;
+	        if (_flashLight != null)
+	        {
+	            _flashLight.transform.eulerAngles = new Vector3(180, _rotationAngles.y, _rotationAngles.z);
+                _flashLight.transform.position = new Vector3(gameObject.transform.position.x - Offset, gameObject.gameObject.transform.position.y, _flashLight.transform.position.z);
+            }
+	    }
+	    if (Input.GetKey(KeyCode.D))
+	    {
+	        _dx++;
+	        if (_flashLight != null)
+	        {
+	            _flashLight.transform.eulerAngles = new Vector3(0, _rotationAngles.y, _rotationAngles.z);
+                _flashLight.transform.position = new  Vector3(gameObject.transform.position.x + Offset,gameObject.gameObject.transform.position.y, _flashLight.transform.position.z);
+            }
+	        
+
+        }
 
 
+	    gameObject.transform.position += new Vector3(_dx * Speed, _dy * Speed,0);
+	    _dx = 0;
+	    _dy = 0;
 
 	}
 
